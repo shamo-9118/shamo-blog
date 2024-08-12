045eee9e1308f7302d1a13f2bfbc884beeb3f815
shamo-9118 <won9118391183@gmail.com>
2024-08-12 21:29:12 +0900

    cardコンポーネント呼び出し

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,14 @@
 import { Heading } from '@/components/Heading';
+import { Card } from '@/components/Card';
 
 export const Main = () => {
   return (
     <main className='bg-[#f5f5f5] min-h-[100vh]'>
       <div className='max-w-[780px] mx-auto pt-[100px] pb-8'>
         <Heading>Blog</Heading>
+      </div>
+      <div className='max-w-[780px] mx-auto'>
+        <Card></Card>
       </div>
     </main>
   );
