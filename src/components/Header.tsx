7b5a5d7cfad3dfb6b22ec3159070f2e1d8b2caed
shamo-9118 <won9118391183@gmail.com>
2024-08-12 21:57:57 +0900

    headerコンポーネント修正

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,22 @@
 export const Header = () => {
-  const navItems = ['about', 'history', 'work'];
+  const navItems = ['About', 'History', 'Work'];
 
   return (
-    <header className='font-mono px-4'>
+    <header className='px-4 text-black'>
       <div className='flex items-end justify-between max-w-[780px] mx-auto pt-8 pb-2'>
-        <p className='text-2xl'>Shamo</p>
+        <p
+          style={{ fontFamily: 'Avenir-Roman, sans-serif' }}
+          className='text-2xl font-bold'
+        >
+          Shamo
+        </p>
         <nav>
           <ul className='flex gap-4 font-bold'>
             {navItems.map((navItem) => {
               return (
                 <li
-                  className='relative hover:text-[#aaa] hover:after:w-full hover:after:transform hover:after:-translate-x-1/2 after:content-[""] after:block after:absolute after:w-0 after:left-1/2 after:-translate-x-1/2 after:bg-[#fff] after:h-[3px] after:rounded-full after:duration-300 after:transition-all after:bottom-[-10px] hover:transition-all hover:after:bg-[#fe2811]'
+                  style={{ fontFamily: 'Avenir-Roman, sans-serif' }}
+                  className='relative hover:text-[#aaa] hover:after:w-full hover:after:transform hover:after:-translate-x-1/2 after:content-[""] after:block after:absolute after:w-0 after:left-1/2 after:-translate-x-1/2 after:bg-[#fff] after:h-[3px] after:rounded-full after:duration-200 after:transition-all after:bottom-[-10px] hover:transition-all hover:after:bg-[#fe2811]'
                   key={navItem}
                 >
                   {navItem}
