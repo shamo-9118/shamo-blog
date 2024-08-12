c894edd0f1d0d9550f593aca37f719e99bd9ca77
shamo-9118 <won9118391183@gmail.com>
2024-08-13 00:02:08 +0900

    githubから記事のデータを取得する関数作成

---
+++ src/hooks/useFetchGithubArticle.tsx
@@ -0,0 +1,48 @@
+import { useCallback, useState } from 'react';
+
+export const useFetchGithubArticle = () => {
+  // NOTE: GitHubから取得できるデータ型が不明のためany
+  // TODO: GitHub側のデータかたわかり次第型をつける
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchGithubArticle = useCallback(async () => {
+    // TODO: ここは引数で受け取れるようにするのが柔軟かもしれない
+    const token = process.env.NEXT_PUBLIC_GITHUB_TOKEN;
+    const url = process.env.NEXT_PUBLIC_GITHUB_ENDPOINT;
+
+    if (!url || !token) {
+      console.error('GitHub API endpoint or token is not defined');
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: 'application/vnd.github.v3+json',
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      setData(result);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setError(error.message);
+      } else {
+        setError('An unknown error occurred');
+      }
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  return { data, error, loading, fetchGithubArticle };
+};
