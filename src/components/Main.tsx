ab1c24d7261ffba8ebe42869dd6e6490dd5721fb
shamo-9118 <won9118391183@gmail.com>
2024-08-12 19:55:41 +0900

    mainコンポーネントでheadingを使用する

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,11 @@
+import { Heading } from '@/components/Heading';
+
 export const Main = () => {
   return (
     <main className='bg-[#f5f5f5] min-h-[100vh]'>
-      <div className='max-w-[780px] mx-auto'>main</div>
+      <div className='max-w-[780px] mx-auto pt-[100px] pb-8'>
+        <Heading>Blog</Heading>
+      </div>
     </main>
   );
 };
