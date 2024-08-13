2c02bda7f3550e9ad556bc1dbe0f9e2abd90ed17
shamo-9118 <won9118391183@gmail.com>
2024-08-13 22:56:57 +0900

    パスの修正

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import type { HeadingProps } from '@/type/Heading';
+import type { HeadingProps } from '@/types/Heading';
 
 export const Heading = (props: HeadingProps) => {
   return <h1 className='text-5xl text-[#111] font-bold'>{props.children}</h1>;
