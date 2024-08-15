bf0b6e997eaf2c9fa67af2f823154f25a7e951e1
shamo-9118 <won9118391183@gmail.com>
2024-08-15 23:14:56 +0900

    記事取得ようの処理作成

--- src/utils/fetchShamoArticleContents.ts
+++ src/utils/fetchShamoArticleContents.ts
@@ -1,24 +1,53 @@
 import { fetchShamoArticleURLs } from '@/utils/fetchShamoArticleURLs';
+import { formatFileNameToData } from './formatFileNameToData';
+import type { ContentData } from '@/types/ContentData';
 
 export const fetchShamoArticleContents = async () => {
   try {
-    const shamoArticleUrls = await fetchShamoArticleURLs();
+    const articleContentTreesData = await fetchShamoArticleURLs();
 
-    if (!shamoArticleUrls) return;
+    if (!articleContentTreesData) return;
 
-    const fetchPromises = shamoArticleUrls.map(async (shamoArticleUrl) => {
-      const res = await fetch(shamoArticleUrl);
+    const fetchPromises = articleContentTreesData.map(
+      async (articleContentTreeData) => {
+        const articleContentPromises =
+          articleContentTreeData.articleContentsTree.map(
+            async (articleContentData) => {
+              const res = await fetch(articleContentData.url);
+              if (!res.ok) {
+                throw new Error(`HTTP Request error: ${res.status}`);
+              }
 
-      if (!res.ok) {
-        throw new Error(`HTTP Request error: ${res.status}`);
-      }
+              const parsedRes: ContentData = await res.json();
 
-      return res.json();
-    });
+              return {
+                res: parsedRes,
+                path: articleContentData.path,
+              };
+            },
+          );
 
-    const allData = await Promise.all(fetchPromises);
+        const articleContentsData = await Promise.all(articleContentPromises);
+        const payloadDrticleContents = articleContentsData.map(
+          (articleContentData) => {
+            return {
+              name: formatFileNameToData(articleContentData.path),
+              content: Buffer.from(
+                articleContentData.res.content,
+                'base64',
+              ).toString('utf-8'),
+            };
+          },
+        );
 
-    return allData;
+        return {
+          name: articleContentTreeData.name.split('_').join('/'),
+          postData: payloadDrticleContents,
+        };
+      },
+    );
+
+    return await Promise.all(fetchPromises);
   } catch (error: unknown) {
     if (error instanceof Error) {
       console.error(
