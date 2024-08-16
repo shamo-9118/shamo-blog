fe2d0c8f3eb3b8be99c827574351383a6f465a21
shamo-9118 <won9118391183@gmail.com>
2024-08-16 14:36:02 +0900

    wip取得したデータを一覧画面作成用に整形

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -4,28 +4,49 @@ import { Loading } from '@/components/Loading';
 import { fetchShamoArticleContents } from '@/utils/fetchShamoArticleContents';
 import { useState, useMemo, useEffect } from 'react';
 
+type ArticleData = {
+  name: string;
+  postData: {
+    content: string;
+    name: string;
+  }[];
+};
+
 export const Main = () => {
-  const [articleData, setArticleData] = useState<any[] | undefined>([]);
+  const [articlesData, setArticleData] = useState<ArticleData[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchArticle = async () => {
-      const articleContents = await fetchShamoArticleContents();
+      const articleContents = await fetchShamoArticleContents(setIsLoading);
+
+      if (!articleContents) return;
       setArticleData(articleContents);
     };
 
     fetchArticle();
   }, []);
 
-  const formatedArticleDataList = useMemo(() => {
-    articleData?.map(() => {});
-  }, []);
+  const formatedArticleListData = useMemo(() => {
+    console.log(
+      articlesData.map((articleData) =>
+        articleData.postData.map((post) => {
+          return {
+            content: post.content,
+            name: post.name,
+          };
+        }),
+      ),
+    );
+  }, [articlesData]);
+
   return (
     <main className='bg-[#f5f5f5] min-h-[100vh]'>
       <div className='max-w-[780px] mx-auto pt-[100px] pb-8'>
         <Heading>Blog</Heading>
       </div>
       <div className='max-w-[780px] mx-auto'>
-        <Card />
+        {isLoading ? <Loading /> : <Card />}
       </div>
     </main>
   );
